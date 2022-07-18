7329317ab43828b981071025537b1e5f18f4c12d
shiyiyi <1910491843@qq.com>
2022-07-18 15:37:25 +0800

    update detail

--- start_alipay.py
+++ start_alipay.py
@@ -7,14 +7,28 @@ from airtest.cli.parser import cli_setup
 if not cli_setup():
     auto_setup(__file__, logdir=True, devices=["Android:///", ])
     from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+
     poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
 
+
 def start():
-    # start_app("com.eg.android.AlipayGphone")
+    start_app("com.eg.android.AlipayGphone")
+    if poco("android:id/button2"):
+        poco("android:id/button2").click()
+
+
+
+    # 闪链签企业HR小程序
     # poco("com.alipay.mobile.antui:id/search_bg").click()
     # text("闪链签企业HR")
     # poco(text="搜索").click()
     # poco(text="闪链签企业HR").click()
-    start_app("com.eg.android.AlipayGphone")
-    poco(text="我的小程序").click()
-    poco(text="闪链签企业HR").click()
+    # stop_app("com.eg.android.AlipayGphone")
+
+    # 闪链签电子单据小程序
+    poco(text="").click()
+    poco("com.alipay.mobile.antui:id/search_bg").click()
+    text("闪链签电子单据")
+    poco(text="搜索").click()
+    poco(text="为组织、企业提供电子单据的发送和管理平").click()
+
---
+++ test_danju_cases/test_Bfaqidanj.py
@@ -0,0 +1,26 @@
+# -*- encoding=utf8 -*-
+import os
+import allure
+import pytest
+
+from common.Unpack import unpack
+from common.operate import Operate_More
+from common.yaml_util import YamlUtil
+
+from common.Screenshots_operation import Screenshots
+
+TestCaseData = YamlUtil().read_extract_yaml(os.getcwd() + "/case_data_danju/faqidanju/shoukuansj.yml")
+
+
+@allure.feature((unpack(TestCaseData))[0])  # 一级菜单
+class Testfaqidanju:
+    @allure.severity(allure.severity_level.CRITICAL)  # 设置用例优先级 用例等级：blocker\critical\normal\minor\trivial
+    @allure.story((unpack(TestCaseData))[1])  # 二级菜单
+    # @allure.title("测试用例标题")  # 测试用例标题
+    @pytest.mark.parametrize('args', TestCaseData)
+    def test_faqi(self, args):
+        try:
+            Operate_More().implement(args["text"])
+        except BaseException as e:
+            print(type(e))
+            print(e)
